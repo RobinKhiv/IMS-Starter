19e4a89ea5ff1edc6344b513619ceb207938c19b
Robin Khiv <robinkhiv@gmail.com>
2021-05-30 23:46:22 -0700

    rk:added item controller and item database access object

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -0,0 +1,45 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ItemDAO implements Dao<item>{
+
+	@Override
+	public List<item> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public item read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public item create(item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public item update(item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public item modelFromResultSet(ResultSet resultSet) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
