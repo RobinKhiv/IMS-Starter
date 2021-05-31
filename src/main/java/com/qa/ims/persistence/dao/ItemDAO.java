6e1a670382288c593ae42f1b7c93799fff722fa0
Robin Khiv <robinkhiv@gmail.com>
2021-05-31 00:10:20 -0700

    rk:added Item class

--- src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -4,8 +4,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-public class ItemDAO implements Dao<item>{
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import com.qa.ims.persistence.domain.Item;
+
+public class ItemDAO implements Dao<Item>{
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+	
 	@Override
 	public List<item> readAll() {
 		// TODO Auto-generated method stub
@@ -34,6 +41,30 @@ public class ItemDAO implements Dao<item>{
 	public int delete(long id) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	@Override
+	public item modelFromResultSet(ResultSet resultSet) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public item read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public item create(item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public item update(item t) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,43 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+	private Long id;
+	private String itemName;
+	private double itemPrice;
+	
+	public Item(String itemName, double itemPrice) {
+		this.setItemName(itemName);
+		this.setItemPrice(itemPrice);
+	}
+	
+	public Item(String itemName, double itemPrice, Long id) {
+		this.setItemName(itemName);
+		this.setItemPrice(itemPrice);
+		this.setId(id);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public double getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(double itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	
+}
