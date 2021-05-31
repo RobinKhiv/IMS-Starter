0778d2774fdcbf3c5c039799f74b0b3d8e7588f0
Robin Khiv <robinkhiv@gmail.com>
2021-05-31 03:18:03 -0700

    rk:added orders methods

---
+++ src/main/java/com/qa/ims/controller/OrdersController.java
@@ -0,0 +1,48 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.OrdersDAO;
+import com.qa.ims.persistence.domain.Orders;
+import com.qa.ims.utils.Utils;
+
+public class OrdersController implements CrudController<Orders> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	private OrdersDAO ordersDAO;
+	private Utils utils;
+	
+	public OrdersController(OrdersDAO orderDAO, Utils utils) {
+		super();
+		this.ordersDAO = orderDAO;
+		this.utils = utils;
+	}
+	@Override
+	public List<Orders> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Orders create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Orders update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/com/qa/ims/persistence/dao/OrdersDAO.java
@@ -0,0 +1,47 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.qa.ims.persistence.domain.Orders;
+
+public class OrdersDAO implements Dao<Orders>  {
+
+	@Override
+	public List<Orders> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Orders read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Orders create(Orders t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Orders update(Orders t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Orders modelFromResultSet(ResultSet resultSet) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
