c054bffd76f7df437d0ceb01e87ca307956256d4
Robin Khiv <robinkhiv@gmail.com>
2021-06-03 08:28:24 -0700

    rk:added method to check for equal to another object

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -47,4 +47,40 @@ public class Item {
 		return "id:" + id + " item name:" + itemName + " price:$" + itemPrice;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(itemPrice);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (itemName == null) {
+			if (other.itemName != null)
+				return false;
+		} else if (!itemName.equals(other.itemName))
+			return false;
+		if (Double.doubleToLongBits(itemPrice) != Double.doubleToLongBits(other.itemPrice))
+			return false;
+		return true;
+	}
+	
 }
