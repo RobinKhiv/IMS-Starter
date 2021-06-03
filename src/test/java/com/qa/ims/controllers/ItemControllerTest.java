379d07dabaf7592f8aec65eb5f085e799b1d26c6
Robin Khiv <robinkhiv@gmail.com>
2021-06-03 08:29:47 -0700

    rk:added test to create item controller

---
+++ src/test/java/com/qa/ims/controllers/ItemControllerTest.java
@@ -0,0 +1,69 @@
+package com.qa.ims.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.qa.ims.controller.ItemController;
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
+public class ItemControllerTest {
+
+	@Mock
+	private Utils utils;
+	
+	@Mock ItemDAO itemDAO;
+	
+	@InjectMocks
+	private ItemController controller;
+	
+	@Test
+	public void ItemControllerConstructorTest() {
+		assertNotNull(controller);
+		assertTrue(controller instanceof ItemController);	
+	}
+	
+	@Test
+	public void readAllTest() {
+		
+	}
+
+	@Test
+	public void createTest() {
+		final String I_NAME = "keyboard";
+		final double I_PRICE = 10d;
+		final Item createdItem = new Item(I_NAME, I_PRICE);
+		
+		Mockito.when(utils.getString()).thenReturn(I_NAME);
+		Mockito.when(utils.getDouble()).thenReturn(I_PRICE);
+		Mockito.when(itemDAO.create(createdItem)).thenReturn(createdItem);
+		
+		assertEquals(createdItem, controller.create());
+		Mockito.verify(utils, Mockito.times(1)).getString();
+		Mockito.verify(utils, Mockito.times(1)).getDouble();
+		Mockito.verify(itemDAO, Mockito.times(1)).create(createdItem);
+	}
+
+	@Test
+	public void updateTest() {
+		
+	}
+
+	@Test
+	public void deleteTest() {
+		
+	}
+}
