2e1e68473b18695496ab87249d8f6e4b08ce8439
Robin Khiv <robinkhiv@gmail.com>
2021-06-04 10:36:19 -0700

    rk:removed hashcode method for item class

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -48,18 +48,6 @@ public class Item {
 	}
 	
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
-		long temp;
-		temp = Double.doubleToLongBits(itemPrice);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		return result;
-	}
-	
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
